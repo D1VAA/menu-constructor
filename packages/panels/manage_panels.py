f8ec8549b2db8029e33ccafc83b90d0ef0c929e1
HyBun <davipare203@gmail.com>
2023-11-06 22:53:41 -0300

    saving

--- packages/panels/manage_panels.py
+++ packages/panels/manage_panels.py
@@ -4,10 +4,9 @@ from ..functions.manage_functions import ManageFunctions
 class ManagePanels(ManageFunctions):
     _instances = {}
     def __init__(self, panel:str):
-        self.panel_name = str(panel)
         self.panel_data = {}
         self.nick = None
-        self.panel_data[self.panel_name] = {'funcs': {}, 'cmds': {}}
+        self.panel_data = {'funcs': {}, 'cmds': {}}
     
     def __new__(cls, *args, **kwargs):
         panel = args[0] if args else kwargs.get('panel')
@@ -19,18 +18,18 @@ class ManagePanels(ManageFunctions):
 
     def add_func(self, nick, func: object, desc = None) -> dict:
         self.nick = nick
-        self.panel_data[self.panel_name]['funcs'][nick] = {'func': func, 'desc': str(desc)}
-        return self.panel_data[self.panel_name]
+        self.panel_data['funcs'][nick] = {'func': func, 'desc': str(desc)}
+        return self.panel_data
     def add_cmds(self, nick, func, desc: str = None):
-        self.panel_data[self.panel_name]['cmds'][nick] = {'func': func, 'desc': str(desc)}
-        return self.panel_data[self.panel_name]
+        self.panel_data['cmds'][nick] = {'func': func, 'desc': str(desc)}
+        return self.panel_data
     
     def _printer(self, opt=None):
-        print(self.panel_data[self.panel_name]['cmds'])
+        print(self.panel_data['cmds'])
         if opt is None:
             print(f'{"=" * 25} COMANDOS {"=" * 25}', end='\n\n')
-            commands = self.panel_data[self.panel_name]['cmds']
-            functions = self.panel_data[self.panel_name]['funcs']
+            commands = self.panel_data['cmds']
+            functions = self.panel_data['funcs']
             for nick, infos in commands.items():
                 description = infos['desc']
                 print(f'{Colors.RED}[-] {nick} {Colors.YELLOW}>{Colors.RESET} {description}', end='\n')
@@ -40,3 +39,16 @@ class ManagePanels(ManageFunctions):
                 print(f'{Colors.CIAN}[+] {nick} {Colors.YELLOW}>{Colors.RESET} {description}', end='\n')
         elif opt is not None: 
             return
+        
+    def run(self):
+        self._printer()
+        while True:
+            opt = str(input('> '))
+            if len(opt.split()) > 1:
+                cmd, f = opt.split()
+                command = self.panel_data['cmds'][cmd]['func']
+                func = self.panel_data['funcs'][f]['func']
+            else:
+                opt = self.panel_data['cmds'][f]['func']
+            if opt.lower() == 'exit':
+                break
