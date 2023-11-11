7f793d4f578360e515b434ca722ae251bc02d345
HyBun <davipare203@gmail.com>
2023-11-11 14:23:03 -0300

    saving

--- packages/panels/manage_panels.py
+++ packages/panels/manage_panels.py
@@ -2,63 +2,74 @@ from ..bcolors import Colors
 
 class ManagePanels:
     _instances = {}
-    def __init__(self, panel:str):
-        self.nick = {}
-        self.panel_data = {}
-        self.panel = str(panel)
-        self.panel_data = {'funcs': {}, 'cmds': {}}
-    
     def __new__(cls, *args, **kwargs):
         panel = args[0] if args else kwargs.get('panel')
-        if panel in cls._instances:
-            return cls._instances[panel]
-        new_instance = super(ManagePanels, cls).__new__(cls)
-        cls._instances[panel] = new_instance
-        return new_instance
+        if panel not in cls._instances.keys(): 
+            new_instance = super().__new__(cls)
+            cls._instances[panel] = new_instance
+            return new_instance
+        return cls._instances[panel]
 
-    def add_func(self, nick, func: object, desc = None):
-        self.panel_data['funcs'][nick] = {'func': func, 'desc': str(desc)}
+    def __init__(self, panel:str):
+        self.nick = {}
+        self.panel = str(panel)
+        self.panel_data = {'opts': {}, 'cmds': {}}
+        self.opts = self.panel_data['opts']
+        self.cmds = self.panel_data['cmds']
+        self.cmds_keys = self.cmds.keys()
+        self.opts_keys = self.opts.keys()
+    
+    @property
+    def instances(self):
+        return self._instances
+
+    def add_opts(self, nick, func: object = None, desc = None):
+        self.opts[nick] = {'func': func, 'desc': str(desc)}
 
     def add_cmds(self, nick, func, desc: str = None):
-        self.panel_data['cmds'][nick] = {'func': func, 'desc': str(desc)}
+        self.cmds[nick] = {'func': func, 'desc': str(desc)}
     
-    def _printer(self, opt=None):
-        if opt is None:
-            print(f'{"=" * 25} COMANDOS {"=" * 25}', end='\n\n')
-            commands = self.panel_data['cmds']
-            functions = self.panel_data['funcs']
-            for nick, infos in commands.items():
+    def printer(self, opt: ['opts', 'cmds']=None):
+        print('\n\n')
+        cmd_format = f'{"=" * 25} COMANDOS {"=" * 25}'
+        opt_format = f'\n{"=" * 26} OPÇÕES {"=" * 26}'
+        if opt == 'cmds':
+            print(cmd_format, end='\n\n')
+            for nick, infos in self.cmds.items():
                 description = infos['desc']
-                print(f'{Colors.RED}[-] {nick} {Colors.YELLOW}>{Colors.RESET} {description}', end='\n')
-            print(f'\n{"=" * 25} FUNÇÕES {"=" * 25}', end='\n\n')
-            for nick, infos in functions.items():
+                print(f'{Colors.RED}[-] {nick} {Colors.RESET}> {description}', end='\n')
+        elif opt == 'opts': 
+            print(opt_format, end='\n\n')
+            for nick, infos in self.opts.items():
                 description = infos['desc']
-                print(f'{Colors.CIAN}[+] {nick} {Colors.YELLOW}>{Colors.RESET} {description}', end='\n')
-            print('\n\n')
-        elif opt is not None: 
-            return
+                print(f'{Colors.BLUE}[+] {nick} {Colors.RESET}> {description}', end='\n')
+        else:
+            print(cmd_format, end='\n\n')
+            for nick, infos in self.cmds.items():
+                description = infos['desc']
+                print(f'{Colors.RED}[-] {nick} {Colors.RESET}> {description}', end='\n')
+            print(opt_format, end='\n\n')
+            for nick, infos in self.opts.items():
+                description = infos['desc']
+                print(f'{Colors.BLUE}[+] {nick} {Colors.RESET}> {description}', end='\n')
+        print('\n\n')
 
     def run(self, input_format=None):
-        self._printer()
-        cmds = self.panel_data['cmds']
-        funcs = self.panel_data['funcs']
+        self.printer()
         df_format = f'({Colors.RED}{self.panel}{Colors.RESET})>' if input_format is None else input_format
         while True:
             opt = input(f'{df_format} ')
             try:
-                if opt not in cmds.keys():
-                        if opt in ['exit', 'quit']:
-                            break
-                        cmd = opt.split()[0]
-                        args = opt.split()[1:] 
-                        
+                if opt in ['exit', 'quit']:
+                    break
+                if opt not in self.cmds_keys:
+                        cmd, *args = opt.split()
+
                         # Replace the nick with the function obj
-                        for i, x in enumerate(args):
-                            if x in funcs.keys():
-                                args[i] = funcs[x]['func']
-                        cmds[cmd]['func'](*args)
+                        args = [self.opts[x]['func'] if (x in self.opts_keys and self.opts[x]['func'] is not None) else x for x in args]
+                        self.cmds[cmd]['func'](*args)
                 else:
-                    cmds[opt]['func']()
+                    self.cmds[opt]['func']()
 
             except Exception as e:
                 print(f'{Colors.RED}[!] ERROR >>> {Colors.RESET}', str(e))
